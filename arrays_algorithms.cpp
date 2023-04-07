7648cd6a1e141953972fdfbd7ba912a2629387e2
mr-shan <55774764+mr-shan@users.noreply.github.com>
2023-04-07 15:44:36 +0530

    Added algorithms related to arrays to find missing elements from the number series.

---
+++ arrays_algorithms.cpp
@@ -0,0 +1,56 @@
+// function to find missing element from the natural elements
+// use the sum to find the missing element here.
+// the formula for that is sum = n(n + 1) / 2
+// if first element is not 1, then use the difference between element and it's index.
+// it should be constant.
+int findMissingElement(int *arr, int length)
+{
+    int firstElement = arr[0], lastElement = arr[length - 1];
+    
+    if (firstElement == 1)
+    {
+        // use the formula n(n + 1) / 2
+        int sum = 0;
+        for (int i = 0; i < length; ++i)
+            sum += arr[i];
+        
+        int expectedSum = (lastElement * (lastElement + 1)) / 2;
+        return expectedSum - sum;
+        
+    } else {
+        // find the difference between the element and it's index
+        int diff = firstElement;
+        
+        for (int i = 1; i < length; ++i)
+        {
+            if (arr[i] - i != diff) {
+                return i + diff;
+            }
+        }
+        return 0;
+    }
+}
+
+// find multiple missing elements from the series of numbers
+void findMultipleMissingElements(int *arr, int length)
+{
+    // get the first and last element. 
+    // also get the difference between first element and first index.
+    int firstElement = arr[0], lastElement = arr[length - 1];
+    int diff = firstElement;
+    
+    for (int i = 1; i < length; ++i)
+    {
+        int currentDiff = arr[i] - i;
+        
+        // if difference between an element and it's index is not `diff`,
+        // we have one or more missing elements. 
+        // number of elements missing is equal to currentDiff - diff
+        // so we can find out missing element by adding diff + i
+        // for more than one missing elements, increment diff till it's equal to currentDiff.
+        if (currentDiff != diff) {
+            while (diff < currentDiff)
+                cout << " " << i + diff++;
+        }
+    }
+}
