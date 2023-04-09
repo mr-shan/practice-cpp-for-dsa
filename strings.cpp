0f4279604547281a2654a92103572d7b24d81f52
mr-shan <55774764+mr-shan@users.noreply.github.com>
2023-04-09 23:37:43 +0530

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -34,6 +34,28 @@ int isPalindrom(char *str)
   return 1;
 }
 
+// This uses bitwise operations to find the duplicate.
+// It makes use of left shifting of bits 
+// Then it uses masking (AND) and merging (OR)
+// to check if duplicate exists.
+int checkDuplicate(char *str)
+{
+  int lenStr = length(str);
+  long int hex = 0, temp = 0;
+  int i = -1;
+  
+  while(str[++i] != '\0')
+  {
+    temp = 1;
+    temp = temp << str[i];
+
+    if ((temp & hex) > 0)
+      printf("Duplicate found: %c\n", str[i]);
+    else
+      hex = hex | temp;
+  }
+}
+
 int main() 
 {
     char name[] = "Madam";
